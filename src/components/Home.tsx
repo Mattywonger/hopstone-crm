0cfc71eb92050bec1590db51e9b589b05fbbcd20
Justin Bravo <justinbravo@Justins-MacBook-Pro.local>
2024-01-11 22:02:47 -0500

    Designed Login Page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,7 @@ import Header from "./header";
 export const Home = () =>
 (
     <div className="flex flex-col items-center justify-center min-h-screen">
-        <div className="text-3xl font-bold underline">Hello, world!</div>
+        {/* <div className="text-3xl font-bold underline">Hello, world!</div> */}
         <Header />
     </div>
 );
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,44 +1,51 @@
-import { useState } from "react"
-import { User } from "../providers/user"
-import { signInWithEmailAndPassword } from "firebase/auth"
-import { useNavigate } from "react-router"
+import { useState } from "react";
+import { User } from "../providers/user";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router";
+import LoginSignupHeader from "./LoginSignupHeader"; 
+import { Button } from "./ui/button"; 
+import { Input } from "./ui/input"; 
+import { Label } from "./ui/label"; 
 
 export const LoginPage = () => {
-    const { auth } = User.useContainer()
+    const { auth } = User.useContainer();
 
-    let [error, setError] = useState<Error | null>()
-    let [email, setEmail] = useState("")
-    let [password, setPassword] = useState("")
+    let [error, setError] = useState<Error | null>();
+    let [email, setEmail] = useState("");
+    let [password, setPassword] = useState("");
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
     const handleSubmit = (event: any) => {
-        event.preventDefault()
+        event.preventDefault();
         signInWithEmailAndPassword(auth, email, password)
             .then(() => navigate("/"))
             .catch((error: Error) => {
-                setError(error)
-            })
+                setError(error);
+            });
     }
 
     return (
         <div>
-            <p>Login</p>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Email:
-                    <input type={"text"} onChange={event => setEmail(event.target.value)} />
-                </label>
-                <label>
-                    Password:
-                    <input type={"password"} onChange={event => setPassword(event.target.value)} />
-                </label>
-                <input type={"submit"} />
-            </form>
-            {error ?
-                <p>An error occurred: {error.message}</p> :
-                <p>All is good</p>
-            }
+            <LoginSignupHeader />
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                <div>
+                    <p style={{ textAlign: 'center', fontWeight: 'bold', color: 'black', fontSize: '1.60rem', marginBottom: '35px' }}>Login</p>
+                    <form onSubmit={handleSubmit}>
+                        <Label htmlFor="email">Email</Label>
+                        <Input id="email" type="text" onChange={event => setEmail(event.target.value)} />
+                        <br />
+                        <Label htmlFor="password">Password</Label>
+                        <Input id="password" type="password" onChange={event => setPassword(event.target.value)} />
+                        <br />
+                        <Button type="submit" variant="default" style={{ marginTop: '20px' }}>Submit</Button>
+                    </form>
+                    {error ?
+                        <p>An error occurred: {error.message}</p> :
+                        <p></p>
+                    }
+                </div>
+            </div>
         </div>
-    )
+    );
 }
