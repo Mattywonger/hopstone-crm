af338fbc5d5dcea1c370db81e1cfaf06df0b714c
Justin Bravo <justinbravo@Justins-MacBook-Pro.local>
2024-01-11 22:41:48 -0500

    Designed Sign Up Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { SignupPage } from './components/signup'
+import { SignupPage } from './components/SignupPage'
 import { Home } from './components/Home'
 import { LoginPage } from './components/LoginPage'
 
