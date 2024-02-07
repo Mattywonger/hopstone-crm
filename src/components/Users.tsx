5256f170dbd221a5c17664c95a11612a558bcd12
Moe Frumkin <moegf1@gmail.com>
2024-02-07 12:43:14 -0500

    update pod data model and interface

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -7,9 +7,8 @@ import Header from "./header";
 import { defaultProfilePic } from "../providers/userProfile"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { addDoc, arrayUnion, deleteDoc, deleteField, doc, setDoc, updateDoc } from "firebase/firestore";
+import { addDoc, arrayRemove, arrayUnion, deleteDoc, deleteField, doc, DocumentReference, updateDoc } from "firebase/firestore";
 import { useState } from "react";
-import { experimentalSetDeliveryMetricsExportedToBigQueryEnabled } from "firebase/messaging/sw";
 
 
 
@@ -33,47 +32,57 @@ export const Users = () => {
         return 0;
     };
 
-    const makePodLeader = (uid: string) => {
+    const makePodLeader = (user: DocumentReference) => {
 
         // TODO: Should have a consistent system for referncing other documents
         // IF not using firestore references can unchain
 
         addDoc(podCollection, {
-            leader: uid,
+            leader: user,
             members: []
         }).then(pod =>
-            updateDoc(doc(firestore, `users/${uid}`), { pod: pod.id }))
+            updateDoc(user, { pod: pod }))
             .catch(setCreatePodError)
     }
 
     // Careful: this takes the UID of the pod leader
-    const deletePod = (uid: string) => {
-        const podid = snapshot?.docs.find(doc => doc.id == uid)?.data().pod
-        const members = pods?.docs.find(doc => doc.id == podid)?.data().members
+    const deletePod = (user: QueryDocumentSnapshot) => {
+        const pod = pods?.docs.find(doc => doc.id == user.data().pod.id)
 
         Promise.all([
-            updateDoc(doc(firestore, `users/${uid}`), { pod: deleteField() }),
-            members.map((memberid: string) => (
-                updateDoc(doc(firestore, `users/${memberid}`), { pod: deleteField() })
+            updateDoc(user.ref, { pod: deleteField() }),
+            pod?.data().members.map((member: DocumentReference) => (
+                updateDoc(member, { pod: deleteField() })
             )),
-            deleteDoc(doc(firestore, `pods/${podid}`))
+            (pod != undefined && deleteDoc(pod.ref))
         ]).catch(setCreatePodError)
     }
 
-    const podLeader = (podid: string) => {
-        const leaderUid = pods?.docs.find(doc => doc.id === podid)?.data().leader
-        if (leaderUid == undefined)
+    const podLeader = (pod: DocumentReference) => {
+        const leaderRef = pods?.docs.find(doc => doc.id === pod.id)?.data().leader
+        if (leaderRef == undefined)
             return undefined;
 
-        else return snapshot?.docs.find(doc => doc.id == leaderUid)
+        else return snapshot?.docs.find(doc => doc.id == leaderRef.id)
 
     }
 
-    const assignToPod = (uid: string, podid: string) => {
+    const assignToPod = (user: QueryDocumentSnapshot, pod: DocumentReference) => {
+        unassign(user)
         Promise.all([
-            updateDoc(doc(firestore, `pods/${podid}`), { members: arrayUnion(uid) }),
-            updateDoc(doc(firestore, `users/${uid}`), { pod: podid })
+            updateDoc(pod, { members: arrayUnion(user.ref) }),
+            updateDoc(user.ref, { pod })
         ]).catch(setCreatePodError)
+    }
+
+    // Remove a user from a pod if they are a member of one
+    const unassign = (user: QueryDocumentSnapshot) => {
+        const pod = user.data().pod
+        if (pod == undefined) return;
+        Promise.all([
+            updateDoc(pod, { members: arrayRemove(user.ref) }),
+            updateDoc(user.ref, { pod: deleteField() })
+        ])
     }
 
     return (
@@ -102,42 +111,50 @@ export const Users = () => {
                                         {snapshot?.docs.sort(sortUsersByLastName).map((user, index, array) => (
                                             <tr key={user.id}>
                                         <td>
-                                                    <img width={50} height={50} src={user.data().profilePic || defaultProfilePic} />
+                                            <img width={50} height={50} src={user.data().profilePic || defaultProfilePic} />
                                         </td>
                                         <td style={{ borderBottom: array.length - 1 === index ? 'none' : '1px solid #ddd', padding: '8px' }}>
-                                                    {user.data().firstName}
-                                                    {user.data().isAdmin && (" (admin) ")}
+                                            {user.data().firstName}
+                                            {user.data().isAdmin && (" (admin) ")}
                                         </td>
                                         <td style={{ borderBottom: array.length - 1 === index ? 'none' : '1px solid #ddd', padding: '8px' }}>
-                                                    {user.data().lastName}
-                                                </td>
-                                                <td>
-                                                    {podLeader(user.data().pod)?.id == user.id ?
-                                                        <>
-                                                            <p>Pod Leader</p>
-                                                            <Button onClick={event => { event.preventDefault; deletePod(user.id) }}>Delete Pod</Button>
-                                                        </> :
-                                                        <>
-                                                            <DropdownMenu>
-                                                                <DropdownMenuTrigger asChild>
-                                                                    <p>{user.data().pod == undefined ? "Not Assigned" : podLeader(user.data().pod)?.data().firstName}</p>
-                                                                </DropdownMenuTrigger>
-                                                                <DropdownMenuContent>
-                                                                    {
-                                                                        pods?.docs.filter(pod => pod.data().leader).map((pod, index, array) => (
-                                                                            <DropdownMenuItem asChild id={pod.id}>
-                                                                                <Button onClick={() => assignToPod(user.id, pod.id)}>{podLeader(pod.id)?.data().firstName}</Button>
-                                                                            </DropdownMenuItem>
-                                                                        ))
-                                                                    }
-                                                                    <DropdownMenuItem asChild>
-                                                                        <Button onClick={() => updateDoc(
-                                                                            doc(firestore, `users/${user.id}`), { pod: deleteField() })
-                                                                            .catch(setCreatePodError)}>Unassign</Button>
+                                            {user.data().lastName}
+                                        </td>
+                                        <td>
+                                            {(user.data().pod && podLeader(user.data().pod)?.id == user.id) ?
+                                                <>
+                                                    <p>Pod Leader</p>
+                                                    <Button onClick={event => { event.preventDefault; deletePod(user) }}>Delete Pod</Button>
+                                                </> :
+                                                <>
+                                                    <DropdownMenu>
+                                                        <DropdownMenuTrigger asChild>
+                                                            <Button>{user.data().pod == undefined ? "Not Assigned" : podLeader(user.data().pod)?.data().firstName}</Button>
+                                                        </DropdownMenuTrigger>
+                                                        <DropdownMenuContent>
+                                                            {
+                                                                pods?.docs.filter(pod => pod.data().leader).map((pod, index, array) => (
+                                                                    <DropdownMenuItem asChild id={pod.id}>
+                                                                        <Button onClick={() => assignToPod(user, pod.ref)}>{podLeader(pod.ref)?.data().firstName}</Button>
                                                                     </DropdownMenuItem>
-                                                                </DropdownMenuContent>
-                                                            </DropdownMenu>
-                                                            <Button onClick={event => { event.preventDefault(); makePodLeader(user.id) }}>Make Pod Leader</Button></>}
+                                                                ))
+                                                            }
+                                                            {user.data().pod != undefined && <DropdownMenuItem asChild>
+                                                                <Button onClick={() => unassign(user)}>Unassign</Button>
+                                                            </DropdownMenuItem>}
+                                                            <DropdownMenuItem asChild>
+                                                                <Button onClick={event => {
+                                                                    event.preventDefault();
+                                                                    unassign(user)
+                                                                    makePodLeader(user.ref)
+                                                                }
+                                                                }>
+                                                                    Make Pod Leader
+                                                                </Button>
+                                                            </DropdownMenuItem>
+                                                        </DropdownMenuContent>
+                                                    </DropdownMenu>
+                                                </>}
                                         </td>
                                         {/* Add other data cells if needed */}
                                     </tr>
