1c266e89f02dec47b8780a71a0dba16e6f091e09
Moe Frumkin <moegf1@gmail.com>
2024-02-11 00:51:18 -0500

    add user collection functions

--- src/lib/users.ts
+++ src/lib/users.ts
@@ -1,9 +1,14 @@
-import { DocumentData, DocumentReference, DocumentSnapshot, Firestore, FirestoreDataConverter, QueryDocumentSnapshot, QuerySnapshot, SnapshotOptions, WithFieldValue, collection } from "firebase/firestore"
+import { CollectionReference, DocumentData, DocumentReference, DocumentSnapshot, Firestore, FirestoreDataConverter, QueryDocumentSnapshot, QuerySnapshot, SnapshotOptions, WithFieldValue, collection } from "firebase/firestore"
 import { ProfilerOnRenderCallback } from "react"
 import { useCollection } from "react-firebase-hooks/firestore"
 
 const defaultProfilePic = "/unknown-user.png";
 const UserCollectionPath = "users"
+
+export type UserCollection = {
+    users: Array<User>,
+    ref: CollectionReference
+}
 
 export type User = {
     profile: UserProfile,
@@ -17,6 +22,17 @@ export type UserProfile = {
     isAdmin: boolean,
     pod: DocumentReference | null
 }
+
+export const useUsers = (firestore: Firestore, path: string): [UserCollection, boolean, Error | null] => {
+    const userCollection = collection(firestore, path).withConverter(userConverter)
+    const [users, loading, error] = useCollection(userCollection)
+
+    return [{ users: users != undefined ? users.docs.map(user => user.data()) : [], ref: userCollection }, loading, error || null]
+}
+
+export const findUser = (ref: DocumentReference, userCollection: UserCollection): User | null => (
+    userCollection.users.find(user => user.ref == ref) || null
+)
 
 const profileFromDoc = (doc: DocumentData) => {
     const data = doc.data()
